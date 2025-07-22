715cbe8c1789ec08f23343a121395a7a2183ea10
Anisha11 <anishas1121@gmail.com>
2025-07-22 17:24:13 +0530

    Switch back to full Excel Analytics app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import TestApp from './TestApp.jsx'
 import './index.css'
 
-// Temporarily use TestApp to debug deployment issues
-// Change back to <App /> once working
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TestApp />
+    <App />
   </React.StrictMode>,
 )
